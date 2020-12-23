34645d83d02c8c42b4d81df24656ad5390617c62
Binbin Mu <dinobunmu@gmail.com>
2020-12-23 12:58:16 -0600

    GraphTest.java test for new euler method as well as lca method

--- GraphTest.java
+++ GraphTest.java
@@ -3,23 +3,60 @@ public class GraphTest {
 
             // Graph.showGraph();
         
-            Graph.addEdge("0", "2");
-            Graph.addEdge("0", "1");
-            // Graph.addEdge("1", "3");
-            Graph.addEdge("1", "2");
-            Graph.addEdge("3", "5");
-            Graph.addEdge("3", "4");
-            Graph.addEdge("3", "2");
-            Graph.addEdge("5", "0");
-            Graph.addEdge("4", "2");
-            // Graph.addEdge("4", "5");
+            Graph.addEdgeOneDirection("1", "2");
+            Graph.addEdgeOneDirection("1", "3");
+            Graph.addEdgeOneDirection("1", "4");
+            Graph.addEdgeOneDirection("2", "5");
+            Graph.addEdgeOneDirection("2", "6");
+            Graph.addEdgeOneDirection("3", "7");
+            Graph.addEdgeOneDirection("4", "8");
+            Graph.addEdgeOneDirection("4", "9");
+            Graph.addEdgeOneDirection("4", "10");
+            Graph.addEdgeOneDirection("5", "11");
+            Graph.addEdgeOneDirection("11", "12");
+            Graph.addEdgeOneDirection("11", "13");
+            Graph.addEdgeOneDirection("7", "14");
+            Graph.addEdgeOneDirection("7", "15");
+            Graph.addEdgeOneDirection("15", "16");
+            Graph.addEdgeOneDirection("15", "17");
+            Graph.addEdgeOneDirection("15", "18");
+
+            // Graph.addEdgeOneDirection("1", "2");
+            // Graph.addEdgeOneDirection("1", "3");
+            // Graph.addEdgeOneDirection("2", "4");
+            // Graph.addEdgeOneDirection("2", "5");
+            // Graph.addEdgeOneDirection("2", "6");
+            // Graph.addEdgeOneDirection("3", "7");
+            // Graph.addEdgeOneDirection("3", "8");
+
+
+
+            // Graph.addEdgeOneDirection("1", "2");
+            // Graph.addEdgeOneDirection("1", "3");
+            // Graph.addEdgeOneDirection("1", "4");
+            // Graph.addEdgeOneDirection("2", "5");
+            // Graph.addEdgeOneDirection("2", "6");
+            // Graph.addEdgeOneDirection("3", "7");
+            // Graph.addEdgeOneDirection("4", "8");
+            // Graph.addEdgeOneDirection("4", "9");
+            // Graph.addEdgeOneDirection("4", "10");
+            // Graph.addEdgeOneDirection("5", "11");
+            // Graph.addEdgeOneDirection("11", "12");
+            // Graph.addEdgeOneDirection("11", "13");
+            // Graph.addEdgeOneDirection("7", "14");
+            // Graph.addEdgeOneDirection("7", "15");
+            // Graph.addEdgeOneDirection("15", "16");
+            // Graph.addEdgeOneDirection("15", "17");
+            // Graph.addEdgeOneDirection("15", "18");
 
             Graph.showGraph();
+            // Graph.euler("1");
+            Graph.lca("9", "12", "1");
 
-            // Graph.dfs("2", "1");
+            // Graph.dfs("1", "5");
 
-            Graph.dfsAll("2");
-            System.out.println(Graph.pathMap);
+            // Graph.dfsAll("2");
+            // System.out.println(Graph.pathMap);
 
             // Graph.bfs("1", "4");
             // Graph.bfsAll("0");
